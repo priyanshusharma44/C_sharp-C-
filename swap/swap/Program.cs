ffdca26509737d6dede03343d6d1c752f0493c85
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2024-07-02 21:47:18 +0545

    Add files via upload

---
+++ swap/swap/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace swap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10; int b = 20;
+            int c = a; //c=10
+             a = b; //a=20
+             b = c; //b=10
+            Console.WriteLine("a:"+ a);
+            Console.WriteLine();
+            Console.WriteLine("b:"+b);
+        }
+    }
+}
