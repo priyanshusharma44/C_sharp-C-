8682b236e7b1498ab97a1ad40f565021b4e5ff96
Priyanshu Sharma <101499662+priyanshusharma44@users.noreply.github.com>
2024-07-02 21:34:53 +0545

    Add files via upload

---
+++ oops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace oops
+{
+    public class Car
+    {
+        string color = "red";  
+        int maxSpeed = 200;         
+        public void fullThrottle()
+        {
+            Console.WriteLine("Color:" + color);
+            Console.WriteLine("MaxSpeed:" + maxSpeed);
+        }
+
+        public static void Main(string[] args)
+        {
+            Car myObj1 = new Car();
+            myObj1.fullThrottle();
+        }
+    }
+
+}
